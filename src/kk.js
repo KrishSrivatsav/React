6d87cdb5c8b8e353232343d749ad158e5e0e066e
krish <krishsrivatsav@gmail.com>
2019-12-20 09:14:27 +0530

    first commit

---
+++ src/kk.js
@@ -0,0 +1,19 @@
+import React from 'react';
+export function Kk() {
+    return (
+        <div class="loginBox">
+			<h2>Log In Here</h2>
+			<form>
+				<p>Email</p>
+				<input type="text" name="" placeholder="Enter Email"/>
+					
+				<p>Password</p>
+				<input type="password" name="" placeholder="••••••"/>
+				<input type="submit" name="" value="Sign In"/>
+
+				{/* <a>Forget Password</a> */}
+			</form>
+	
+		</div>
+     );
+}
